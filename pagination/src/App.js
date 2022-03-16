066a9fe0c6f9a226a43deff02806d7bd0b3f7337
gourav <gvirus21yt@gmail.com>
2022-03-16 13:40:00 +0530

    pagination implemented

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,19 +1,50 @@
 import "./App.css";
+import { useState } from "react";
 import UserCard from "./components/userCard";
-import Mockdata from "./MOCKDATA.json";
+import JSONData from "./MOCKDATA.json";
+import ReactPaginate from "react-paginate";
 
 function App() {
+  const [users, setUsers] = useState(JSONData.slice(0, 60));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <div className="outer-container">
+          <UserCard
+            key={user.id}
+            firstName={user.first_name}
+            lastName={user.last_name}
+          />
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className="App">
-      <div className="outer-container">
-        {Mockdata.map((val) => (
-          <UserCard
-            key={val.id}
-            firstName={val.first_name}
-            lastName={val.last_name}
-          />
-        ))}
-      </div>
+      {displayUsers}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBtns"}
+        previousLinkClassName={"previousBtn"}
+        nextLinkClassName={"nextBtn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 }

da519c02063116e1e7cbad4e0d3ddd9ed74854db
gourav <gvirus21yt@gmail.com>
2022-03-16 13:16:34 +0530

    added simple gradient card ui

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import UserCard from "./components/userCard";
-import Mockdata from "./MOCKDATA.json"
+import Mockdata from "./MOCKDATA.json";
 
 function App() {
   return (
     <div className="App">
-      {
-        Mockdata.map((val) => (
-          <UserCard key={val.id} firstName={val.first_name} lastName={val.last_name}/>
-        ))
-      }
-      <UserCard firstName="John" lastName="Doe"/>
+      <div className="outer-container">
+        {Mockdata.map((val) => (
+          <UserCard
+            key={val.id}
+            firstName={val.first_name}
+            lastName={val.last_name}
+          />
+        ))}
+      </div>
     </div>
   );
 }

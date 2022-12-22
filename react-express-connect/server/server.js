d59ced2fb0ced36bd02e426b239c0f72011af71e
gvirus21 <gouravkumar21.dev@gmail.com>
2022-12-22 19:39:12 +0530

    created the basic users api

---
+++ react-express-connect/server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/users", (req, res) => {
+  res.json(
+     [
+      {
+        id: 1,
+        name: "john",
+      },
+      {
+        id: 2,
+        name: "alice",
+      },
+      {
+        id: 3,
+        name: "Bob",
+      },
+      {
+        id: 4,
+        name: "Hitesh",
+      },
+      {
+        id: 5,
+        name: "Lu",
+      },
+    ],
+  );
+});
+
+app.listen(4000, () => {
+  console.log("Server started on http://localhost:4000");
+});

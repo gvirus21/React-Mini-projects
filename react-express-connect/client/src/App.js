11bc1a0f9f2a71de73d5e8d8daaf222028b8882a
gvirus21 <gouravkumar21.dev@gmail.com>
2022-12-22 20:32:28 +0530

    final commit

--- react-express-connect/client/src/App.js
+++ react-express-connect/client/src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:4000/api/users");
+      setUsers(res.data);
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
-      App
+      <div className="container">
+        <h2>Users</h2>
+        {users?.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- react-express-connect/server/server.js
+++ react-express-connect/server/server.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
 
 app.get("/api/users", (req, res) => {
-  res.json(
-     [
-      {
-        id: 1,
-        name: "john",
-      },
-      {
-        id: 2,
-        name: "alice",
-      },
-      {
-        id: 3,
-        name: "Bob",
-      },
-      {
-        id: 4,
-        name: "Hitesh",
-      },
-      {
-        id: 5,
-        name: "Lu",
-      },
-    ],
-  );
+  res.json([
+    {
+      id: 1,
+      name: "john",
+    },
+    {
+      id: 2,
+      name: "alice",
+    },
+    {
+      id: 3,
+      name: "Bob",
+    },
+    {
+      id: 4,
+      name: "Hitesh",
+    },
+    {
+      id: 5,
+      name: "Lu",
+    },
+  ]);
 });
 
 app.listen(4000, () => {
